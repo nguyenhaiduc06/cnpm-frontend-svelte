24e53cd996153a702b70c1fb0f1668dc7380a9e4
Nguyen Hai Duc <nguyenhaiduc06@gmail.com>
2023-02-06 16:51:33 +0700

    fix: pocketbase url

--- src/utils/ApiClient.js
+++ src/utils/ApiClient.js
@@ -1,20 +1,20 @@
 import PocketBase, { LocalAuthStore, Admin } from "pocketbase";
 // ---
-import CommonHelper      from "@/utils/CommonHelper";
-import { replace }       from "svelte-spa-router";
+import CommonHelper from "@/utils/CommonHelper";
+import { replace } from "svelte-spa-router";
 import { addErrorToast } from "@/stores/toasts";
-import { setErrors }     from "@/stores/errors";
-import { setAdmin }      from "@/stores/admin";
+import { setErrors } from "@/stores/errors";
+import { setAdmin } from "@/stores/admin";
 
 /**
  * Clears the authorized state and redirects to the login page.
  *
  * @param {Boolean} [redirect] Whether to redirect to the login page.
  */
- PocketBase.prototype.logout = function(redirect = true) {
+PocketBase.prototype.logout = function (redirect = true) {
     this.authStore.clear();
     if (redirect) {
-        replace('/login');
+        replace("/login");
     }
 };
 
@@ -25,17 +25,17 @@ import { setAdmin }      from "@/stores/admin";
  * @param  {Boolean} notify     Whether to add a toast notification.
  * @param  {String}  defaultMsg Default toast notification message if the error doesn't have one.
  */
- PocketBase.prototype.errorResponseHandler = function(err, notify = true, defaultMsg = '') {
+PocketBase.prototype.errorResponseHandler = function (err, notify = true, defaultMsg = "") {
     if (!err || !(err instanceof Error) || err.isAbort) {
         return;
     }
 
-    const statusCode = (err?.status << 0) || 400;
+    const statusCode = err?.status << 0 || 400;
     const responseData = err?.data || {};
 
     // add toast error notification
     if (
-        notify &&          // notifications are enabled
+        notify && // notifications are enabled
         statusCode !== 404 // is not 404
     ) {
         let msg = responseData.message || err.message || defaultMsg;
@@ -58,7 +58,7 @@ import { setAdmin }      from "@/stores/admin";
     // forbidden
     if (statusCode === 403) {
         this.cancelAllRequests();
-        return replace('/');
+        return replace("/");
     }
 };
 
@@ -85,7 +85,7 @@ class AppAuthStore extends LocalAuthStore {
     }
 }
 
-const client = new PocketBase("http://127.0.0.1:8090", new AppAuthStore("pb_admin_auth"));
+const client = new PocketBase("https://cnpm.fly.dev/", new AppAuthStore("pb_admin_auth"));
 
 if (client.authStore.model instanceof Admin) {
     setAdmin(client.authStore.model);
