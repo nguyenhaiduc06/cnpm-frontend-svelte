a6395e4b0b6795c5d7092d29893a62bc5bb12db9
Nguyen Hai Duc <nguyenhaiduc06@gmail.com>
2023-02-09 14:53:43 +0700

    update: stats sidebar links

--- src/components/stats/StatsSidebar.svelte
+++ src/components/stats/StatsSidebar.svelte
@@ -21,21 +21,5 @@
         >
             <span class="txt">Lịch sử thay đổi nhân khẩu</span>
         </a>
-        <a
-            href="/stats/rewards"
-            class="sidebar-list-item"
-            use:link
-            use:active={{ path: "/stats/rewards", className: "active" }}
-        >
-            <span class="txt">Khen thưởng</span>
-        </a>
-        <a
-            href="/stats/gifts"
-            class="sidebar-list-item"
-            use:link
-            use:active={{ path: "/stats/gifts", className: "active" }}
-        >
-            <span class="txt">Trao quà</span>
-        </a>
     </div>
 </aside>
